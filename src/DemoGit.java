88109cb7fcd172d6ad261e4cdce07fa0d3a03a02
bitam <mazloombita@foothill.edu>
2019-04-08 17:39:34 -0700

    Initial course material

---
+++ src/DemoGit.java
@@ -0,0 +1,23 @@
+/**
+ * Simple class to test submitting to GitHub repository.
+ * @author Foothill College, Bita M
+ *
+ * Note: If you modified this file then update the author name with your: FirstName LastName
+ * 
+ * Reminder: Take a look at "Example Program Submission" in course Modules and follow the
+ *           "Program Guidelines" to make sure your submission is complete.
+ */
+public class DemoGit 
+{
+
+	/**
+	 * Outputs my name	
+	 * @param args [describe the args here]
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello. My name is [Enter your name here]");
+
+	}
+
+}
